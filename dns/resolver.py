d761b63ac40e666bed8771a0e5b38d7062cdb954
YoctoByte <david.vtwout@gmail.com>
2016-05-12 12:26:39 +0200

    update

--- dns/resolver.py
+++ dns/resolver.py
@@ -9,7 +9,6 @@ DNS server, but with a different list of servers.
 
 import socket
 from threading import Thread
-# from dns.cache import RecordCache
 from dns.cache import RecordCache
 from dns.classes import Class
 from dns import message
@@ -48,7 +47,7 @@ class Resolver(object):
         self.STYPE = Type.A
         self.SCLASS = Class.IN
         self.SLIST = []
-        self.SBELT = ['8.8.8.8'] # TODO: slist moet eigenlijk geinitialiseerd worden vanuit een config file
+        self.SBELT = ['8.8.8.8']  # TODO: slist moet eigenlijk geinitialiseerd worden vanuit een config file
         if(self.caching):
             self.CACHE = RecordCache(self.ttl)
 
@@ -59,38 +58,47 @@ class Resolver(object):
 
         best_servers = self.find_best_servers()
 
-        def send_query(server_address):
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            sock.settimeout(timeout)
-
-            # Create and send query
-            question = message.Question(hostname, Type.A, Class.IN)
-            header = message.Header(9001, 0, 1, 0, 0, 0)
-            header.qr = 0
-            header.opcode = 0
-            header.rd = 1
-            query = message.Message(header, [question])
-            sock.sendto(query.to_bytes(), ('8.8.8.8', 53))
-
-            # Receive response
-            data = sock.recv(512)
-            response = message.Message.from_bytes(data)
-
-            # Get data
-            aliases = list()
-            for additional in response.additionals:
-                if additional.type_ == Type.CNAME:
-                    aliases.append(additional.rdata.data)
-            addresses = list()
-            for answer in response.answers:
-                if answer.type_ == Type.A:
-                    addresses.append(answer.rdata.data)
+        # Get data
+        aliases = list()
+        for additional in response.additionals:
+            if additional.type_ == Type.CNAME:
+                aliases.append(additional.rdata.data)
+        addresses = list()
+        for answer in response.answers:
+            if answer.type_ == Type.A:
+                addresses.append(answer.rdata.data)
 
         for server in best_servers:
-            response = Thread(target=send_query, args=(best_servers,))
+            Thread(target=send_query, args=(server,)).start()
 
         return hostname, aliases, addresses
 
+    # step 2:
     def find_best_servers(self):
         # todo: implement this function
         return self.SLIST
+
+    # step 3:
+    def send_query(self, server_address, timeout):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.settimeout(timeout)
+
+        # Create and send query
+        question = message.Question(hostname, Type.A, Class.IN)
+        header = message.Header(9001, 0, 1, 0, 0, 0)
+        header.qr = 0
+        header.opcode = 0
+        header.rd = 1
+        query = message.Message(header, [question])
+        sock.sendto(query.to_bytes(), (server_address, 53))
+
+        # Receive response
+        data = sock.recv(512)
+        response = message.Message.from_bytes(data)
+
+    # step 4:
+    def analyze_response(self, response):
+        # if response answers question or contains name error: chache data and return back to client
+
+        #
+        pass
