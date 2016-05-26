a909ee433eb75bbdee3fdee6acb6bea3689243a0
YoctoByte <david.vtwout@gmail.com>
2016-05-26 12:28:39 +0200

    resolver aan gewerkt

--- dns/resolver.py
+++ dns/resolver.py
@@ -8,14 +8,15 @@ DNS server, but with a different list of servers.
 """
 
 import socket
-# from threading import Thread
-from dns.cache import RecordCache
+from threading import Thread
+# from Queue import Queue
+from dns.cache import RecordCache, MockedCache
 from dns.classes import Class
 from dns import message
 # from dns.rcodes import RCode
 from dns.types import Type
 from dns.resource import ResourceRecord
-# import time
+from time import sleep
 
 
 # todo: implement pending_requests: pending_requests is een lijst met "state blocks" die een tijd, wat parameters en de SLIST bevatten
@@ -31,15 +32,16 @@ class Resolver(object):
             caching (bool): caching is enabled if True
             ttl (int): ttl of cache entries (if > 0)
         """
-        self.caching = caching
         self.ttl = ttl
-        self.STYPE = Type.A
-        self.SCLASS = Class.IN
-        self.SLIST = ['8.8.8.8', '198.41.0.4']
-        self.SBELT = ['198.41.0.4']  # TODO: slist moet eigenlijk geinitialiseerd worden vanuit een config file
+        self.caching = caching
         if self.caching:
             self.CACHE = RecordCache(self.ttl)
-        self.pending_requests = []
+        else:
+            self.CACHE = MockedCache(self.ttl)
+        self.STYPE = Type.A
+        self.SCLASS = Class.IN
+        self.SLIST = [(None, '8.8.8.8'), (None, '198.41.0.4')]
+        self.SBELT = [(None, '8.8.8.8'), (None, '198.41.0.4')]  # TODO: slist moet eigenlijk geinitialiseerd worden vanuit een config file
         self.aliases = []
         self.addresses = []
         self.timeout = 3
@@ -58,10 +60,9 @@ class Resolver(object):
         self.addresses = []
 
         # Step 1 of rfc 1034 sect 5.3.3:
-        if self.caching:
-            answer = self.CACHE.lookup(self.SNAME, Type.A, Class.IN)
-            if answer:
-                return self.SNAME, answer.aliases, answer.addresses
+        answer = self.CACHE.lookup(self.SNAME, Type.A, Class.IN)
+        if answer:
+            return self.SNAME, answer.aliases, answer.addresses
 
         # step 2:
         self.update_slist()
@@ -74,40 +75,77 @@ class Resolver(object):
     # step 2:
     def update_slist(self):
         # todo: implement this function
-        self.SLIST = ['198.41.0.4']  # 198.41.0.4
+        self.SLIST = [(None, '198.41.0.4')]  # 198.41.0.4
         # example:
         # if SNAME is Mockapetris.ISI.EDU, first look for a NS RRs
         # for Mockapetris.ISI.EDU, then ISI.EDU, then EDU and then . (root)
 
     # step 3:
     def send_queries(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.settimeout(self.timeout)
-
-        # Create and send query
+        # Create query
         question = message.Question(self.SNAME, Type.A, Class.IN)
-        header = message.Header(9002, 0, 1, 0, 0, 0)
+        header = message.Header(9001, 0, 1, 0, 0, 0)
         header.qr = 0
         header.opcode = 0
         header.rd = 1
         query = message.Message(header, [question])
         query_bytes = query.to_bytes()
 
-        for server in self.SLIST:
-            sock.sendto(query_bytes, (server, 53))
+        def send_query(ind, server_data):
+            server_name = server_data[0]
+            server_ip = server_data[1]
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.settimeout(self.timeout)
+            if not server_ip:
+                if server_name:
+                    ns_resolver = Resolver(self.caching, self.ttl)
+                    _, addresses, _ = ns_resolver.gethostbyname(server_name)
+                    if addresses:
+                        server_ip = addresses[0]
+                    else:
+                        results[ind] = -1
+                        return
+                else:
+                    results[ind] = -1
+                    return
 
-        # Receive response
-        try:
-            data = sock.recv(512)
-            response = message.Message.from_bytes(data)
-            print('--------')
-            self.show_response(response)
-            self.analyze_response(response)
-        except socket.timeout:
-            print('timeout')
+            try_count = 0
+            max_tries = 3
+            while try_count < max_tries:
+                print(server_ip)
+                sock.sendto(query_bytes, (server_ip, 53))
+                try:
+                    results[ind] = (sock.recv(512), server_data)
+                    return
+                except socket.timeout:
+                    print('timeout ' + str(try_count))
+                    try_count += 1
+            results[ind] = -1
+
+        results = list()
+        for ind, server in enumerate(self.SLIST):
+            results.append(None)
+            Thread(target=send_query, args=(ind, server)).start()
+
+        while True:
+            if not all(r in [None, -1] for r in results):  # if result is found
+                response = None
+                server_data = None
+                for r in results:
+                    if r not in [None, -1]:
+                        response = message.Message.from_bytes(r[0])
+                        server_data = r[1]
+                        break
+                print('--------\nserver:' + str(server_data[0]))
+                self.show_response(response)
+                self.analyze_response(response, server_data)
+            if all(r == -1 for r in results):  # all results timed out
+                # todo: do something here
+                pass
+            sleep(0.01)
 
     # step 4:
-    def analyze_response(self, response):
+    def analyze_response(self, response, server_data):
         # step 4:
         self.SLIST = []
         additionals = list()
@@ -126,22 +164,19 @@ class Resolver(object):
         for authority in response.authorities:
             rr = ResourceRecord(authority.name, authority.type_, authority.class_, authority.ttl, authority.rdata)
             if rr.type_ == Type.NS:
-                # found_ip = False
+                found_ip = False
                 for additional in additionals:
                     if additional.name == rr.rdata.data and additional.type_ == Type.A:
-                        self.SLIST.append(additional.rdata.data)
-                #         found_ip = True
-                # if not found_ip:
-                #     _, addresses, _ = self.gethostbyname(rr.rdata.data)
-                #     self.SLIST.append(addresses)
-
+                        self.SLIST.append((rr.rdata.data, additional.rdata.data))
+                        found_ip = True
+                if not found_ip:
+                    self.SLIST.append((rr.rdata.data, None))
             if rr.type_ == Type.A and rr.class_ == Class.IN:
                 pass  # for IN class, data is a 32 bit ip address
             if rr.type_ == Type.CNAME:
-                self.gethostbyname(rr.rdata.data)
-                return
+                new_sname = rr.rdata.data
+                return self.gethostbyname(new_sname)
         self.send_queries()
-
 
     @staticmethod
     def show_response(response):
@@ -157,7 +192,7 @@ class Resolver(object):
             print 'additional:', additional.rdata.data, Type.by_value[additional.type_], Class.by_value[
                 additional.class_], additional.name, additional.ttl
 
-if __name__ == "__main__": # anders wordt onderstaande gerunt op het moment dat deze klasse
+if __name__ == "__main__":  # anders wordt onderstaande gerunt op het moment dat deze klasse
     # wordt geimporteerd
     resolver = Resolver(False, 3600)
-    resolver.gethostbyname('www.opaalstraat.nl')
+    resolver.gethostbyname('www.google.com')
